35d230146d40de580f001ceab124815f4170e15e
HusseinSpi <Huseeink852@gmail.com>
2024-07-21 17:08:53 +0300

    music2

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -3,14 +3,7 @@ const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getAllMusic = catchAsync(async (req, res, next) => {
-  const { type } = req.query;
-  let query = {};
-
-  if (type) {
-    query.type = type;
-  }
-
-  const Musics = await Music.find(query);
+  const Musics = await Music.find({});
   res.status(200).json({
     status: "success",
     data: Musics,
